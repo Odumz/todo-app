431db7b7e6a97aa2b181c5681c9ffa10ee3bfb60
Odumz <odumz20@gmail.com>
2021-01-29 01:22:17 +0100

    initial views with data

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,5 +1,11 @@
 <template>
     <div>
-        <p class="tw-font-bold">Welcome</p>
+        <p class="tw-font-bold">Welcome {{title}}</p>
     </div>
 </template>
+
+<script>
+export default {
+    props: ['title', 'description']
+}
+</script>
