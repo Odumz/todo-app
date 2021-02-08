59f2d895130eeb9042ce8699d5d22269fbb6c04f
Odumz <odumz20@gmail.com>
2021-02-08 12:48:35 +0100

    edit function added, not updating though

--- resources/js/components/addTodoForm.vue
+++ resources/js/components/addTodoForm.vue
@@ -3,8 +3,8 @@
         <div class="tw-mb-4">
             <!-- <h1 class=" tw-text-gray-700">Todo List</h1> -->
             <div class="tw-flex tw-mt-4">
-                <input v-model="todo.title" class="tw-ring-1 tw-appearance-none tw-rounded-full tw-bg-blue-100 tw-border tw-border-green-300 hover:tw-border-green-500 focus:tw-border-green-500 tw-py-2 tw-px-3 tw-mr-4 tw-text-gay-800" placeholder="Add Todo"/>
-                <button :class="[ todo.title ? 'active' : 'inactive']" class="tw-flex-no-shrink tw-p-2 tw-border-2 tw-rounded-full tw-bg-white" @click="storeTodo()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
+                <input v-model="todo.title" class="tw-outline-none tw-ring-1 tw-appearance-none tw-rounded-full tw-bg-blue-100 tw-border tw-border-green-300 hover:tw-border-green-500 focus:tw-border-green-500 tw-py-2 tw-px-3 tw-mr-4 tw-text-gay-800" placeholder="Add Todo"/>
+                <button :class="[ todo.title ? 'active' : 'inactive', 'tw-outline-none']" class="tw-flex-no-shrink tw-p-2 tw-border-2 tw-rounded-full tw-bg-white" @click="storeTodo()"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
             </div>
         </div>        
     </div>
--- resources/js/components/listTodo.vue
+++ resources/js/components/listTodo.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="container tw-flex tw-flex-row">
         <div class="tw-w-5/6">
-            <input v-model="todo.done" :class="[todo.done ? 'done' : '', 'tw-mx-2']"  type="checkbox" @change="updateCheck()" class="">
-            <label :class="[todo.done ? 'done' : '']" for="one">{{ todo.title }}</label>
+            <input v-if="!this.edit" v-model="todo.done" :class="[todo.done ? 'done' : '', 'tw-mx-2']"  type="checkbox" @change="updateCheck()">
+            <label v-if="!this.edit" :class="[todo.done ? 'done' : '']" for="one">{{ todo.title }}</label>
+            <input v-if="this.edit" v-model="todo.title" class="tw-border-b-2 tw-border-green-400 tw-w-full tw-outline-none" type="text" value="todo.title" @change="updateCheck()" @click="edit()">
+            <!-- <label :class="[todo.done ? 'done' : '']" for="one">{{ todo.title }}</label> -->
         </div>
-        <div class="tw-w-1/6">
+        <div class="tw-w-1/6 tw-flex tw-flex-row">
+            <button v-if="!this.edit" class="tw-flex-no-shrink tw-p-2 tw-ml-2 tw-rounded tw-text-blue-400 hover:tw-text-blue-700" @click="editTodo(todo.id)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-3"><path d="M12 20h9"></path><path d="M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4L16.5 3.5z"></path></svg>
+            </button>
+            <button v-if="this.edit" class="tw-flex-no-shrink tw-p-2 tw-ml-2 tw-rounded tw-text-green-400 hover:tw-text-green-700" @click="updateTodo()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-save"><path d="M19 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h11l5 5v11a2 2 0 0 1-2 2z"></path><polyline points="17 21 17 13 7 13 7 21"></polyline><polyline points="7 3 7 8 15 8"></polyline></svg>
+            </button>
             <button class="tw-flex-no-shrink tw-p-2 tw-ml-2 tw-rounded tw-text-red-400 hover:tw-text-red-700" @click="removeTodo()">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
             </button>
@@ -16,6 +24,11 @@ import axios from 'axios'
 
 export default {
     props: ['todo'],
+    data() {
+        return {
+            edit: false,
+        }
+    },
     methods: {
         updateCheck() {
             // this.done = !this.done
@@ -43,6 +56,23 @@ export default {
             .catch ( error => {
                 console.log(error);
             })
+        },
+        updateTodo() {
+            axios.put(`api/todo/`+ this.todo.id, {
+                todo: this.todo
+            })
+            .then ( response => {
+                if (response.status == 200 ) {
+                    this.$emit('todochanged');
+                    return this.edit = false;
+                }
+            })
+            .catch ( error => {
+                console.log(error);
+            })
+        },
+        editTodo($id) {
+            return this.edit = true;
         }
     },
 }
